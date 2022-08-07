c74c1cb08b7acc20a5676c6e9537ee2c677d34ec
javiervs.dev <javiervs.dev@gmail.com>
2022-08-07 22:48:23 +0200

    docs: Añade javadoc a los servicios

--- addition/src/main/java/dev/javiervs/addition/service/AdditionService.java
+++ addition/src/main/java/dev/javiervs/addition/service/AdditionService.java
@@ -6,5 +6,13 @@ import dev.javiervs.common.exception.OperandException;
 import java.math.BigDecimal;
 
 public interface AdditionService {
+
+    /**
+     * Adds two operands and returns the result.
+     *
+     * @param request the request containing the two operands
+     * @return the result of the addition
+     * @throws OperandException if either operand is null
+     */
     BigDecimal add(AdditionRequest request) throws OperandException;
 }
--- subtraction/src/main/java/dev/javiervs/subtraction/service/SubtractionService.java
+++ subtraction/src/main/java/dev/javiervs/subtraction/service/SubtractionService.java
@@ -6,5 +6,13 @@ import dev.javiervs.subtraction.dto.SubtractionRequest;
 import java.math.BigDecimal;
 
 public interface SubtractionService {
+
+    /**
+     * Subtracts two operands and returns the result.
+     *
+     * @param request the request containing the two operands
+     * @return the result of the subtraction
+     * @throws OperandException if either operand is null
+     */
     BigDecimal subtract(SubtractionRequest request) throws OperandException;
 }
