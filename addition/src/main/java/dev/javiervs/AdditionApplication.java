59a21bb7e2aa534feea5fce10f34bdd761467663
javiervs.dev <javiervs.dev@gmail.com>
2022-08-07 16:25:36 +0200

    feat: Crea el módulo addition

---
+++ addition/src/main/java/dev/javiervs/AdditionApplication.java
@@ -0,0 +1,11 @@
+package dev.javiervs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdditionApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdditionApplication.class, args);
+    }
+}
